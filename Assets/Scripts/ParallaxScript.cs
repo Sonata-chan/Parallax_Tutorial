3c92df109024a7aab52326c7ad9b68e8deffee57
Aluno <aluno@naverecife.local>
2019-09-10 09:59:46 -0300

    First Commit

---
+++ Assets/Scripts/ParallaxScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParallaxScript : MonoBehaviour
+{
+    public RawImage img;
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rect rect = img.uvRect;
+        rect.x += speed;
+        img.uvRect = rect;
+    }
+}
